81349f30970df449ad3e50e38b8d8b957294d51f
Samuel Ackon <ackons433@gmail.com>
2025-02-28 17:10:36 +0000

    Add dropdown exports for Resources, Solutions, and Products in Header

---
+++ src/components/Header/dropdowns/index.ts
@@ -0,0 +1,3 @@
+export { default as ResourcesDropdown } from './ResourcesDropdown';
+export { default as SolutionsDropdown } from './SolutionsDropdown';
+export { default as ProductsDropdown } from './ProductsDropdown';
