d698ac788112684cf97f9900038c21006aa0189b
Samuel Ackon <ackons433@gmail.com>
2025-02-28 17:09:53 +0000

    Add index file for Header component to streamline exports

---
+++ src/components/Header/index.ts
@@ -0,0 +1,14 @@
+import Header from './Header';
+
+export default Header;
+
+// Export individual components for direct import
+export { default as Logo } from './Logo';
+export { default as MainNav } from './MainNav';
+export { default as NavItems } from './NavItems';
+export { default as SearchBar } from './SearchBar';
+export { default as AuthButtons } from './AuthButtons';
+export { default as MobileMenu } from './MobileMenu';
+
+// Export dropdown components
+export * from './dropdowns';
