f3562bcd89a2dba762e6206e2a0b805a09282798
Samuel Ackon <ackons433@gmail.com>
2025-03-01 02:08:32 +0000

    Add index file for Grid components to streamline exports

---
+++ src/components/Grid/index.ts
@@ -0,0 +1,5 @@
+export { default as Grid } from './Grid';
+export { default as GridCell } from './GridCell';
+export { default as GridGuides } from './GridGuides';
+export { default as GridSystem } from './GridSystem';
+export { default as GridBlock } from './GridBlock';
