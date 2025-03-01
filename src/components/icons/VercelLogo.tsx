c7bee17707f4733f94bb645f558e6c71f4461c85
Samuel Ackon <ackons433@gmail.com>
2025-03-01 01:39:36 +0000

    Add VercelLogo component for displaying the Vercel logo in the footer

---
+++ src/components/icons/VercelLogo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const VercelLogo: React.FC = () => (
+  <svg 
+    width="16" 
+    height="16" 
+    viewBox="0 0 16 16" 
+    fill="currentColor" 
+    aria-label="Vercel Logo"
+  >
+    <path 
+      fillRule="evenodd" 
+      clipRule="evenodd" 
+      d="M8 1L16 15H0L8 1Z" 
+    />
+  </svg>
+);
+
+export default VercelLogo;
