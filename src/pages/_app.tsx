1104cfe1af3dedbe0ba1b9220ff899fa338467e2
Samuel Ackon <ackons433@gmail.com>
2025-03-01 01:39:41 +0000

    Add Footer component to include footer section in the application layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@/context/ThemeContext';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
