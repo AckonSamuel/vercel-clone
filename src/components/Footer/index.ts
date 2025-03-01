a4e5d5da6f6be350dfcd97438388a9eb0dabd3c4
Samuel Ackon <ackons433@gmail.com>
2025-03-01 01:38:54 +0000

    Add Footer index file to export components for easier access

---
+++ src/components/Footer/index.ts
@@ -0,0 +1,6 @@
+import Footer from './Footer';
+
+export default Footer;
+export { FooterGroup } from './FooterGroup';
+export { StatusIndicator } from './StatusIndicator';
+export { ThemeSwitcher } from './ThemeSwitcher';
